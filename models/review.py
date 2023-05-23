d68c9a09461cb31e6330e924c60828e26bc3ad6d
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-05-22 23:45:05 -0400

    Fixing doctrings

--- models/review.py
+++ models/review.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+"""Review module for the HBNB project."""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
+    """Review classto store review information."""
+
     place_id = ""
     user_id = ""
     text = ""
