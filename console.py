8636221d6327c5e438bc0ea4791612e395f7a234
JimmyHernandez <jimoem24@gmail.com>
2023-05-29 14:22:18 -0400

    change def all

--- console.py
+++ console.py
@@ -232,11 +232,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in new_obj._FileStorage__objects.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in new_obj._FileStorage__objects.items():
                 print_list.append(str(v))
 
         print(print_list)
