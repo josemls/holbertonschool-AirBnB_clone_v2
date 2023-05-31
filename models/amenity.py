94a6fe00d624a84b62a202df5167fc6440831950
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-05-30 23:16:28 -0400

    Adding place_amenities instance, line 14-15

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """The Amenity class inherits from the BaseModel and Base/
+    """The Amenity class inherits from the BaseModel and Base\
     classes in Python."""
 
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        'Place', secondary='place_amenities', back_populates='amenities')
