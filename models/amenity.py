2d64246aa8235ac6103cabba4a2a6ed73c70be55
JimmyHernandez <jimoem24@gmail.com>
2023-06-03 17:50:15 -0400

    add code

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """State Module for HBNB project."""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String
+from os import getenv
 from sqlalchemy.orm import relationship
 
 
@@ -9,5 +11,8 @@ class Amenity(BaseModel, Base):
     """The Amenity class inherits from the BaseModel and Base\
     classes in Python."""
 
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
