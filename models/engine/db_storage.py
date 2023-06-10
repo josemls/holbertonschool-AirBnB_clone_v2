ac7ed4ddf283a84cdc5c48aff110ba79ec0c96b7
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-10 16:12:15 -0400

    Adding close method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -20,9 +20,7 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """
-        This is the constructor method for a Python class.
-        """
+        """Initialize a Python class."""
         user = getenv('HBNB_MYSQL_USER')
         password = getenv('HBNB_MYSQL_PWD')
         host = getenv('HBNB_MYSQL_HOST')
@@ -39,10 +37,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """
-        This is a method definition in a Python class that takes an optional/
-        argument 'cls'."""
-
+        """Define a Python class that takes an optional argument 'cls'."""
         session = self.__session
         classes = [User, State, City, Amenity, Place, Review]
 
@@ -62,43 +57,41 @@ class DBStorage:
 
     def new(self, obj):
         """
-        The function "new" takes in an object as a parameter.
+        Take in an object as a parameter.
 
         :param obj: The "obj" parameter in the "new" method is typically
         used to create a new instance of a class. It represents the object
         that is being created and initialized. The "self" parameter, on the
         other hand, refers to the instance of the class that the method is
-        being called on
+        being called on.
         """
         self.__session.add(obj)
 
     def save(self):
-        """
-        The function "save" is defined, but its implementation is not shown.
-        """
+        """Save information."""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """
-        This function deletes an object from a session in Python.
+        """Delete an object from a session in Python.
 
         :param obj: The "obj" parameter is an optional argument that
         represents the object to be deleted from the database. If this
         parameter is not provided, the method will not do anything. If
         it is provided, the method will use the SQLAlchemy session to
-        delete the object from the database
+        delete the object from the database.
         """
 
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """
-        The function "reload" is not defined and therefore cannot be/
-        summarized.
-        """
+        """Reload information."""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Close session."""
+        self.__session.close()
