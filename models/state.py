e58b4ba72aacf95216fe894753d48cb8940f76b1
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-11 08:43:06 -0400

    Uploading state.py file

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if os.environ.get('HBNB_TYPE_STORAGE') != 'db':
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship('City', cascade='all, delete', backref='states')
 
     else:
