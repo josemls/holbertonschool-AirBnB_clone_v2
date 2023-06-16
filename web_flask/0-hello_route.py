4fd71318ba6fef9be529c4d0d55d9f60f4a06cb4
josemls <jose_9513@hotmail.com>
2023-06-16 12:02:02 -0400

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Script to start a Flask application."""
+
+"""Script that starts a Flask"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return a given message."""
-    return "Hello HBNB!"
-
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,1 +1,1 @@
-# Flask 
+web flask
