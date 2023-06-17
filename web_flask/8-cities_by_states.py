a9c14ca4d37b03c5145830b073e87b2c2e2342ec
josemls <jose_9513@hotmail.com>
2023-06-17 02:25:56 -0400

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application."""
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
+from models import State
 
 
 app = Flask(__name__)
 
 
+
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
+def states_list():
+    """ State list """
+    states = storage.all("State")
+    states = [state for state in states.values()]
+    return render_template("8-cities_by_states.html", states=states)
 
-    return render_template('cities_by_states.html', states=sorted_states)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    
+    app.run(host="0.0.0.0", port=5000, debug=True)
