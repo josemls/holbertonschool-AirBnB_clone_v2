6d6621f9859c5664512cafae511998be25a6f276
josemls <jose_9513@hotmail.com>
2023-06-17 02:22:45 -0400

    cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
-from flask import Flask, render_template
-from models import storage
-from models import State
-
-
-app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
-#!/usr/bin/python3
-"""Script that starts Flask"""
+"""Script that starts a Flask web application."""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -38,14 +24,4 @@ def cities_by_states():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-@app.route('/cities_by_states', strict_slashes=False)
-def states_list():
-    """ State list """
-    states = storage.all("State")
-    states = [state for state in states.values()]
-    return render_template("8-cities_by_states.html", states=states)
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    
